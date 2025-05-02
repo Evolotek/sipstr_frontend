9a941ba0a557d8b14dfc4369af7d933175b3943e
Gaurav Agrawal <gauravagrawal@priyankas-MacBook-Pro.local>
2025-05-02 17:39:53 +0530

    Home

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import CommonTextView from '../../components/CommonTextView';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-     Home Screen
+     <CommonTextView>Home</CommonTextView>
     </View>
   );
 }
